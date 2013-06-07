39d8773dbecb4235d120cb9d762f97969c225368
Art Covert <thx1138@Anu.(none)>
2013-06-07 08:40:55 -0500

    Added asexual population. Asexual population supports lineage tracking and customizable fields from an arbitrary detail dump
    Modified evaluator to be more flexilable/reliable
    -supports FullLandscape 1
    -deletes landscaping dat file for avida error detection
    -properly supports puting evaluator files in a sub directory for clean living
    
    test_lineage.py contains an example of using both of these classes to landscape everything on a line of descent
    
    readme.txt for test_linage implementation notes

---
+++ landscape-test/test_landscape.py
@@ -0,0 +1,21 @@
+from avida_support.asexual_population import *
+from avida_support.evaluator import *
+
+#create an asexual population
+lineage = cASexualPopulation("avida_files/data/detail-10000.spop",fields={"parent":3,"living":4,"genome":16})
+
+#need an evaluator instance to run landscaping
+evaluator = MutationEvaluator("avida_files/")
+
+#functions to test the evaluator
+#print evaluator.basic_robustness("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+#print evaluator.basic_robustness("wkjagczvvvkvyucuryyuvuavycteyAafuumsydzvfcaxgab")
+
+#traces the line of descent -- returns nothing
+lineage.trace_line_of_descent()
+
+#look at everything on the line of descent from the final dominant to the ancestor
+for x in lineage.walk_line_of_descent():
+    #print the genome id and the robustness analysis
+    print x[0],evaluator.basic_robustness(x[1]['genome'])
+    
