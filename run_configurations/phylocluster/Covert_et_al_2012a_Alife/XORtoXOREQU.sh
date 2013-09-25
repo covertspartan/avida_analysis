de837051aa87b106225db47600d572746413c6da
Art Covert <covertar@Ninhursag.(none)>
2013-09-24 22:27:56 -0500

    Added files for Jared's alife paper

---
+++ run_configurations/phylocluster/Covert_et_al_2012a_Alife/XORtoXOREQU.sh
@@ -0,0 +1,47 @@
+
+#!/bin/bash
+
+# SGE Options
+#$ -S /bin/bash
+
+##Name of the run
+#$ -N XtoXE
+
+##Maximum amount of time the run can go
+#$ -l h_rt=72:00:00
+
+##how many to run?
+##These numbers correspond directly to the random number seed
+#$ -t 1-200
+
+##Which computes nodes to run on.
+##This should never change unless Art tells you so
+#$ -q wilke
+
+# Create Working Directory
+WDIR=/state/partition1/$USER/$JOB_NAME-$JOB_ID-$SGE_TASK_ID
+mkdir -p $WDIR
+if [ ! -d $WDIR ]
+then
+  echo $WDIR not created
+  exit
+fi
+cd $WDIR
+
+# Copy Data and Config Files
+cp /home/jmc4939/config/* .
+
+# Put your Science related commands here
+# Run avida, use the TASK_ID as the random number seed
+# Redirect standard io and error io to the file "run.log"
+./avida -s $SGE_TASK_ID -c XOREQUavida.cfg -set EVENT_FILE XORcontrolRunWithPunish-469340-$(($SGE_TASK_ID%25+1))events.cfg >& run.log
+
+
+
+# Copy Results Back to Home Directory
+RDIR=$HOME/LocalExperiments/$JOB_NAME-$JOB_ID-$SGE_TASK_ID
+mkdir -p $RDIR
+cp -r * $RDIR/. 
+
+# Cleanup 
+rm -rf $WDIR
